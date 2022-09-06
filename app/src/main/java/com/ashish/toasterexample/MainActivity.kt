ea8255c71097558712b07e87d87ea1077eed8c8b
ashish-python <44824214+ashish-python@users.noreply.github.com>
2022-09-06 18:09:53 -0400

    Module settings for jitpack

--- app/src/main/java/com/ashish/toasterexample/MainActivity.kt
+++ app/src/main/java/com/ashish/toasterexample/MainActivity.kt
@@ -2,10 +2,12 @@ package com.ashish.toasterexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ashish.toasterlibrary.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ToasterMessage.showToast(this, "")
     }
 }
